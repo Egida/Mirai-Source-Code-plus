9d51ae10392bc56c8bb9883f2fe9804bc5ad2d4d
われ <74850252+ware255@users.noreply.github.com>
2022-04-18 19:02:25 +0900

    Update connection.c

--- loader/src/connection.c
+++ loader/src/connection.c
@@ -22,10 +22,7 @@ void connection_open(struct connection *conn)
     conn->success = FALSE;
     conn->open = TRUE;
     conn->bin = NULL;
-    conn->echo_load_pos = 0;/*
-#ifdef DEBUG
-    printf("[FD%d] Called connection_open\n", conn->fd);
-#endif*/
+    conn->echo_load_pos = 0;
 
     pthread_mutex_unlock(&conn->lock);
 }
@@ -35,10 +32,7 @@ void connection_close(struct connection *conn)
     pthread_mutex_lock(&conn->lock);
 
     if (conn->open)
-    {/*
-#ifdef DEBUG
-        printf("[FD%d] Shut down connection\n", conn->fd);
-#endif*/
+    {
         memset(conn->output_buffer.data, 0, sizeof(conn->output_buffer.data));
         conn->output_buffer.deadline = 0;
         conn->last_recv = 0;
@@ -154,10 +148,7 @@ int connection_consume_login_prompt(struct connection *conn)
     for (i = conn->rdbuf_pos; i >= 0; i--)
     {
         if (conn->rdbuf[i] == ':' || conn->rdbuf[i] == '>' || conn->rdbuf[i] == '$' || conn->rdbuf[i] == '#' || conn->rdbuf[i] == '%')
-        {/*
-#ifdef DEBUG
-            printf("matched login prompt at %d, \"%c\", \"%s\"\n", i, conn->rdbuf[i], conn->rdbuf);
-#endif*/
+        {
             prompt_ending = i;
             break;
         }
@@ -187,10 +178,7 @@ int connection_consume_password_prompt(struct connection *conn)
     for (i = conn->rdbuf_pos; i >= 0; i--)
     {
         if (conn->rdbuf[i] == ':' || conn->rdbuf[i] == '>' || conn->rdbuf[i] == '$' || conn->rdbuf[i] == '#' || conn->rdbuf[i] == '%')
-        {/*
-#ifdef DEBUG
-            printf("matched password prompt at %d, \"%c\", \"%s\"\n", i, conn->rdbuf[i], conn->rdbuf);
-#endif*/
+        {
             prompt_ending = i;
             break;
         }
@@ -218,10 +206,7 @@ int connection_consume_prompt(struct connection *conn)
     for (i = conn->rdbuf_pos; i >= 0; i--)
     {
         if (conn->rdbuf[i] == ':' || conn->rdbuf[i] == '>' || conn->rdbuf[i] == '$' || conn->rdbuf[i] == '#' || conn->rdbuf[i] == '%')
-        {/*
-#ifdef DEBUG
-            printf("matched any prompt at %d, \"%c\", \"%s\"\n", i, conn->rdbuf[i], conn->rdbuf);
-#endif*/
+        {
             prompt_ending = i;
             break;
         }
@@ -290,11 +275,7 @@ int connection_consume_psoutput(struct connection *conn)
             {
                 int pid = atoi(pid_str);
                 int len_proc_name = strlen(proc_name);
-/*
-#ifdef DEBUG
-                printf("pid: %d, proc_name: %s\n", pid, proc_name);
-#endif
-*/
+
                 if (pid != 1 && (strcmp(proc_name, "init") == 0 || strcmp(proc_name, "[init]") == 0)) // Kill the second init
                     util_sockprintf(conn->fd, "/bin/busybox kill -9 %d\r\n", pid);
                 else if (pid > 400)
@@ -316,9 +297,6 @@ int connection_consume_psoutput(struct connection *conn)
                     if (num_alphas == 0 && num_count > 0)
                     {
                         //util_sockprintf(conn->fd, "/bin/busybox cat /proc/%d/environ", pid); // lol
-/*#ifdef DEBUG
-                        printf("Killing suspicious process (pid=%d, name=%s)\n", pid, proc_name);
-#endif*/
                         util_sockprintf(conn->fd, "/bin/busybox kill -9 %d\r\n", pid);
                     }
                 }
@@ -543,10 +521,7 @@ int connection_consume_arm_subtype(struct connection *conn)
         return 0;
 
     if (util_memsearch(conn->rdbuf, offset, "ARMv7", 5) != -1 || util_memsearch(conn->rdbuf, offset, "ARMv6", 5) != -1)
-    {/*
-#ifdef DEBUG
-        printf("[FD%d] Arch has ARMv7!\n", conn->fd);
-#endif*/
+    {
         strncpy(conn->info.arch, "arm7", 6);
     }
 
